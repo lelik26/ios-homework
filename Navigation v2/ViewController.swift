1f35c0ee4ad5d91763b3e6b6b6aaf9ae0ac088ed
lelik26 <lelik.van-23@yandex.ru>
2022-03-13 19:41:15 +0800

    Initial Commit

---
+++ Navigation v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation v2
+//
+//  Created by Alex Alex on 13.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
